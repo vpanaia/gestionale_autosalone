80a8ffddf088be61139dae5cedc9bf97faaece51
vpanaia <vinc.panaia@gmail.com>
2024-11-05 10:04:43 +0100

    Added Employee and User entity classes

---
+++ src/main/java/com/apuliadigital/gestionaleautosalone/employee/Employee.java
@@ -0,0 +1,140 @@
+package com.apuliadigital.gestionaleautosalone.employee;
+
+import com.apuliadigital.gestionaleautosalone.department.Department;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees", schema = "gestionale_autosalone")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "birth_place", nullable = false)
+    private String birthPlace;
+
+    @Column(name = "tax_code", nullable = false)
+    private String taxCode;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+
+    @Column(name = "hire_date", nullable = false)
+    private LocalDate hireDate;
+
+    @ColumnDefault("current_timestamp()")
+    @Column(name = "created", nullable = false)
+    private Instant created;
+
+    @ColumnDefault("current_timestamp()")
+    @Column(name = "updated")
+    private Instant updated;
+
+    @Column(name = "deleted")
+    private Instant deleted;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getTaxCode() {
+        return taxCode;
+    }
+
+    public void setTaxCode(String taxCode) {
+        this.taxCode = taxCode;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    public Instant getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Instant updated) {
+        this.updated = updated;
+    }
+
+    public Instant getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Instant deleted) {
+        this.deleted = deleted;
+    }
+
+}
